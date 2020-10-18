faad00cb3bb6f31dd34fb0c8f142b2d7ad79bb44
Glenn Posadas <hello@glennvon.com>
2020-10-19 01:34:29 +0800

    reindent profiledatastore

--- HealthKitQuickFamiliarizationTest/ProfileDataStore.swift
+++ HealthKitQuickFamiliarizationTest/ProfileDataStore.swift
@@ -20,20 +20,46 @@ class ProfileDataStore {
             let birthdayComponents =  try hkStore.dateOfBirthComponents()
             let biologicalSex = try hkStore.biologicalSex()
             let bloodType = try hkStore.bloodType()
-              
+            
             let today = Date()
             let calendar = Calendar.current
             let todayDateComponents = calendar.dateComponents([.year],
-                                                                from: today)
+                                                              from: today)
             let thisYear = todayDateComponents.year!
             let age = thisYear - birthdayComponents.year!
-             
+            
             let unwrappedBiologicalSex = biologicalSex.biologicalSex
             let unwrappedBloodType = bloodType.bloodType
-              
+            
             return (age, unwrappedBiologicalSex, unwrappedBloodType)
         }
     }
     
-    
+    class func getMostRecentSample(for sampleType: HKSampleType,
+                                   completion: @escaping (HKQuantitySample?, Error?) -> Swift.Void) {
+        let mostRecentPredicate = HKQuery.predicateForSamples(withStart: Date.distantPast,
+                                                              end: Date(),
+                                                              options: .strictEndDate)
+        
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate,
+                                              ascending: false)
+        
+        let limit = 1
+        
+        let sampleQuery = HKSampleQuery(sampleType: sampleType,
+                                        predicate: mostRecentPredicate,
+                                        limit: limit,
+                                        sortDescriptors: [sortDescriptor]) { (query, samples, error) in
+            guard let samples = samples,
+                  let mostRecentSample = samples.first as? HKQuantitySample else {
+                
+                completion(nil, error)
+                return
+            }
+            
+            completion(mostRecentSample, nil)
+        }
+        
+        HKHealthStore().execute(sampleQuery)
+    }
 }
