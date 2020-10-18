4e9f6d723ceb39bef990a8f096fafde2cf0374c9
Glenn Posadas <hello@glennvon.com>
2020-10-18 18:19:55 +0800

    start HKSetupAssistant.

--- HealthKitQuickFamiliarizationTest/HKSetupAssistant.swift
+++ HealthKitQuickFamiliarizationTest/HKSetupAssistant.swift
@@ -23,3 +23,36 @@ extension HealthkitSetupError: LocalizedError {
         }
     }
 }
+
+class HKSetupAssistant {
+    class func authorizeHK(completion: @escaping (Bool, Error?) -> ()) {
+        guard HKHealthStore.isHealthDataAvailable() else {
+            completion(false, HealthkitSetupError.notAvailable)
+            return
+        }
+        
+        guard let dob = HKObjectType.characteristicType(forIdentifier: .dateOfBirth),
+              let bloodType = HKObjectType.characteristicType(forIdentifier: .bloodType),
+              let biologicalSex = HKObjectType.characteristicType(forIdentifier: .biologicalSex),
+              let bodyMassIndex = HKObjectType.quantityType(forIdentifier: .bodyMassIndex),
+              let height = HKObjectType.quantityType(forIdentifier: .height),
+              let bodyMass = HKObjectType.quantityType(forIdentifier: .bodyMass),
+              let activeEnergy = HKObjectType.quantityType(forIdentifier: .activeEnergyBurned) else {
+            
+            completion(false, HealthkitSetupError.dataTypeNotAvailable)
+            return
+        }
+        
+        let healthKitTypesToWrite: Set<HKSampleType> = [bodyMassIndex,
+                                                        activeEnergy,
+                                                        HKObjectType.workoutType()]
+            
+        let healthKitTypesToRead: Set<HKObjectType> = [dob,
+                                                       bloodType,
+                                                       biologicalSex,
+                                                       bodyMassIndex,
+                                                       height,
+                                                       bodyMass,
+                                                       HKObjectType.workoutType()]
+    }
+}
